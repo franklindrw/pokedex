9c34ae7951143c50d517d962c521d6ebda51e96f
franklindrw <franklindrw@gmail.com>
2022-03-02 21:59:29 -0300

    primeiro commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Pokedex',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
